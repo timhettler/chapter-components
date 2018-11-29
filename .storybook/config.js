209158422e17087d202e19eeeadd59ec060d15f7
Tim Hettler <me@timhettler.com>
2018-11-29 14:13:50 -0500

    add info addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { withOptions } from '@storybook/addon-options';
 
 const req = require.context('../packages', true, /\.stories\.js$/);
 
 addDecorator(withInfo);
+
+addDecorator(
+  withOptions({
+    name: 'Chapter Components',
+    url: 'https://gitlab.ny.rga.com/samsung-within/chapter-components',
+  })
+);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
