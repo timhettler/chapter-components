b0434809a16dcebee47551e78e2a01ed473cfc22
Tim Hettler <me@timhettler.com>
2018-11-29 14:01:21 -0500

    add storybook info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 const req = require.context('../packages', true, /\.stories\.js$/);
+
+addDecorator(withInfo);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
