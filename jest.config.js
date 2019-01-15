551deef6a8c36fe3d3c5977e527ee1545a0a4b47
Tim Hettler <me@timhettler.com>
2019-01-15 13:57:00 -0500

    ignore boilerplate test script

--- jest.config.js
+++ jest.config.js
@@ -138,9 +138,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ['/node_modules/', '/scripts/'],
 
   // The regexp pattern Jest uses to detect test files
   // testRegex: "",
