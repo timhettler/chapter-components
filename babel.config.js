1ed5c0ed6b668be7445f7b7da3547ac239b0e824
Tim Hettler <me@timhettler.com>
2019-01-16 17:19:48 -0500

    Update babel config use .browserslistrc

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          esmodules: true,
-        },
-        modules: false,
-      },
-    ],
-    '@babel/preset-react',
-  ],
-  env: {
-    test: {
-      presets: [['@babel/preset-env'], '@babel/preset-react'],
-    },
-  },
+  presets: [['@babel/preset-env'], '@babel/preset-react'],
 };
