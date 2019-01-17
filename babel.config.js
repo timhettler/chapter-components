71eef7ba06bfb4ce5227b44c8818615edc217cd9
Tim Hettler <me@timhettler.com>
2019-01-17 15:27:10 -0500

    dont use es modules when testing

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [['@babel/plugin-transform-runtime', { useESModules: true }]],
+  env: {
+    test: {
+      plugins: [['@babel/plugin-transform-runtime', { useESModules: false }]],
+    },
+  },
 };
