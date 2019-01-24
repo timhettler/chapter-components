2802d65b95488f07cf8d3c0b1ceb6fc1682c0751
Tim Hettler <me@timhettler.com>
2019-01-24 16:15:15 -0500

    add requried props to test component

--- packages/next-chapter/src/__tests__/index.test.js
+++ packages/next-chapter/src/__tests__/index.test.js
@@ -3,6 +3,8 @@ import Component from '../';
 import renderer from 'react-test-renderer';
 
 it(`${Component.name} renders correctly`, () => {
-  const tree = renderer.create(<Component />).toJSON();
+  const tree = renderer
+    .create(<Component title="FOO" subtitle="BAR" imgSrc="BUZ" />)
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
