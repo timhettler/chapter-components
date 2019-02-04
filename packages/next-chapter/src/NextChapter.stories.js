c9367d3435633927df84b6e8fc589dc8a6d250e0
Tim Hettler <me@timhettler.com>
2019-02-04 18:01:50 -0500

    update to scale based on width

--- packages/next-chapter/src/NextChapter.stories.js
+++ packages/next-chapter/src/NextChapter.stories.js
@@ -8,15 +8,20 @@ const stories = storiesOf('Bookend', module);
 stories.addDecorator(withKnobs);
 
 stories.add('NextChapter', () => (
-  <NextChapter
-    href={text('Link', 'https://www.samsung.com/uk/explore/samsung-within-5/')}
-    title={text('Title', 'Next: Chapter 5')}
-    subtitle={text('Subtitle', 'More than a Stylus')}
-    disabled={boolean('Disabled', false)}
-    theme={radios('Theme', { Dark: 'dark', Light: 'light' }, 'dark')}
-    imgSrc={text(
-      'Image Url',
-      'https://www.foot.com/wp-content/uploads/2017/03/placeholder.gif'
-    )}
-  />
+  <div style={{ width: '314px' }}>
+    <NextChapter
+      href={text(
+        'Link',
+        'https://www.samsung.com/uk/explore/samsung-within-5/'
+      )}
+      title={text('Title', 'Next: Chapter 5')}
+      subtitle={text('Subtitle', 'More than a Stylus')}
+      disabled={boolean('Disabled', false)}
+      theme={radios('Theme', { Dark: 'dark', Light: 'light' }, 'dark')}
+      imgSrc={text(
+        'Image Url',
+        'https://www.foot.com/wp-content/uploads/2017/03/placeholder.gif'
+      )}
+    />
+  </div>
 ));
