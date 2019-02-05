a9b56f8c40ef00e040bf9e9fa23f34f54fddc2b2
Tim Hettler <me@timhettler.com>
2019-02-05 12:28:59 -0500

    update snapshot

--- packages/disclaimer/src/__tests__/index.test.js
+++ packages/disclaimer/src/__tests__/index.test.js
@@ -3,6 +3,6 @@ import Block from '../';
 import renderer from 'react-test-renderer';
 
 it(`${Block.name} renders correctly`, () => {
-  const tree = renderer.create(<Block copy="FOO" />).toJSON();
+  const tree = renderer.create(<Block>FOO</Block>).toJSON();
   expect(tree).toMatchSnapshot();
 });
