485ea817522eeeb4a6e3ccb401494043e59629ff
Tim Hettler <me@timhettler.com>
2019-02-05 12:59:58 -0500

    update failing tests

--- packages/background-transition/src/__tests__/index.test.js
+++ packages/background-transition/src/__tests__/index.test.js
@@ -3,6 +3,10 @@ import Component from '../';
 import renderer from 'react-test-renderer';
 
 it(`${Component.name} renders correctly`, () => {
-  const tree = renderer.create(<Component imageList={[]} />).toJSON();
+  const tree = renderer.create(<Component images={[
+  'https://images.pexels.com/photos/1842623/pexels-photo-1842623.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+]} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
