f088541727e2534b200bf649af965d0dc8eb254c
Tim Hettler <me@timhettler.com>
2019-02-07 11:02:55 -0500

    remove unused import

--- packages/navigation/src/Navigation.stories.js
+++ packages/navigation/src/Navigation.stories.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, boolean, radios } from '@storybook/addon-knobs';
 import Navigation from '.';
-import { Close, Icon } from '@ssgw/icon';
 
 const stories = storiesOf('Bookend', module);
 
