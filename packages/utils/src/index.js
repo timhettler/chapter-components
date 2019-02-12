4d8cf2e8ec12af96389f767f96444e557f416f1d
Tim Hettler <me@timhettler.com>
2019-02-12 15:40:58 -0500

    Add match media hoc for chapters to ingest

--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -1,3 +1,4 @@
 export { default as prefersReducedMotion } from './js/prefersReducedMotion';
 export { default as getEmailUrl } from './js/getEmailUrl';
 export { default as ssgwPropTypes } from './js/propTypes';
+export { default as matchMediaConnect } from './js/matchMediaConnect';
