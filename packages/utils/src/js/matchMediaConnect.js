baf354ccd632b88f97c68799a1b1886e34b93406
Hassane Fall <hassane.fall@rga.com>
2019-03-12 12:35:26 -0400

    change breakpoints

--- packages/utils/src/js/matchMediaConnect.js
+++ packages/utils/src/js/matchMediaConnect.js
@@ -1,21 +1,20 @@
 import { createMatchMediaConnect } from 'react-matchmedia-connect';
 
 const breakpoint = {
-  small: 375,
+  small: 320,
   medium: 768,
 };
 
 const connect = createMatchMediaConnect({
-  isExtraSmall: '(max-width: 320px) and (max-height: 568px)',
-  isSmall: `(max-width: ${breakpoint.small}px)`,
+  isSmall: `(min-width: ${
+    breakpoint.small
+  }px) and (max-width: ${breakpoint.small + 160}px)`,
   isSmallAndMedium: `(max-width: ${breakpoint.medium}px)`,
-  isMedium: `(min-width: ${breakpoint.small + 1}px) and (max-width: ${
+  isMedium: `(min-width: ${breakpoint.small + 161}px) and (max-width: ${
     breakpoint.medium
   }px)`,
   isMediumAndLarge: `(min-width: ${breakpoint.medium + 1}px)`,
-  isLaptopScreen: `(min-width: ${
-    breakpoint.medium
-  }px) and (max-height: ${breakpoint.medium + 12}px)`,
+  isSmallHeight: `(max-height: ${breakpoint.medium + 12}px)`,
 });
 
 export default connect;
