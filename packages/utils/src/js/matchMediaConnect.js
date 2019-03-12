deb5f076ee8bd175873123e061c7d5b4f8fa89bf
Hassane Fall <hassane.fall@rga.com>
2019-03-12 12:01:22 -0400

    add breakpoints to media queries

--- packages/utils/src/js/matchMediaConnect.js
+++ packages/utils/src/js/matchMediaConnect.js
@@ -8,13 +8,14 @@ const breakpoint = {
 const connect = createMatchMediaConnect({
   isExtraSmall: '(max-width: 320px) and (max-height: 568px)',
   isSmall: `(max-width: ${breakpoint.small}px)`,
-  isMax500: '(max-width: 500px)',
   isSmallAndMedium: `(max-width: ${breakpoint.medium}px)`,
   isMedium: `(min-width: ${breakpoint.small + 1}px) and (max-width: ${
     breakpoint.medium
   }px)`,
   isMediumAndLarge: `(min-width: ${breakpoint.medium + 1}px)`,
-  isLaptopScreen: '(min-width: 768px) and (max-height: 780px)',
+  isLaptopScreen: `(min-width: ${
+    breakpoint.medium
+  }px) and (max-height: ${breakpoint.medium + 12}px)`,
 });
 
 export default connect;
